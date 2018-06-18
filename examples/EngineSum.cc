38752d3a5a156cd615d2442af3299a3dd133a918
Nishant Mehta <nishant@clearsky-data.com>
2018-06-18 11:20:00 -0400

    Adding Sum example.

---
+++ examples/EngineSum.cc
@@ -0,0 +1,38 @@
+#include "EngineSum.h"
+#include <stdexcept>
+#include <memory>
+#include "EngineFactory.h"
+#include "EngineRegistrationHelper.h"
+
+static EngineRegistrationHelper<EngineSum>
+  engineRegistration(
+    "sum",
+    { EngineFactory::ENGINE_IN_OPT_NUMBER_LIST,
+      EngineFactory::ENGINE_IN_OPT_FILE_LIST});
+
+
+double EngineSum::run()
+{
+  double result;
+
+  if (!_mPOperandStream->getNext(result))
+  {
+    throw std::runtime_error("Null input");
+  }
+
+  double operand;
+
+  while (_mPOperandStream->getNext(operand))
+  {
+    result += operand;
+  }
+
+  return (result);
+}
+
+// static
+std::unique_ptr<Engine> EngineSum::make(std::unique_ptr<OperandStream> aPOperandStream)
+{
+  std::unique_ptr<Engine> p(new EngineSum(std::move(aPOperandStream)));
+  return p;
+}
---
+++ examples/EngineSum.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <memory>
+#include "Engine.h"
+#include "EngineFactory.h"
+
+/**
+ * Impelments an Engine to sum a stream of numbers.
+ */
+class EngineSum : public Engine
+{
+public:
+
+  /**
+   *
+   * @param aPOperandStream
+   */
+  EngineSum(std::unique_ptr<OperandStream> aPOperandStream):
+    Engine(),
+    _mPOperandStream(std::move(aPOperandStream))
+  { }
+
+  /**
+   * A factory method to instantiate this engine.
+   *
+   * @param aPOperandStream: Input stream for the engine. The caller yields
+   * ownership of the operandStream and expects the engine to free it,
+   * when the engine is destroyed.
+   * @return The constructured engine, expects the caller to take
+   * ownership of this engine and free it when its done.
+   *
+   */
+  static std::unique_ptr<Engine> make(std::unique_ptr<OperandStream> aPOperandStream);
+
+  virtual double run() final;
+
+  virtual ~EngineSum() {}
+
+private:
+
+  Engine& operator =(const Engine &);
+
+  EngineSum(EngineSum &);
+
+  std::unique_ptr<OperandStream> _mPOperandStream;
+
+};
