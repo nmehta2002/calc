11e25082e139767ce51a1a7a0738e7ca526e5434
Nishant Mehta <nishant@clearsky-data.com>
2018-06-18 19:03:50 -0400

    One more.

--- run_integ_tsts.py
+++ run_integ_tsts.py
@@ -11,7 +11,7 @@ assert(result == 2)
 print 'Divider command line input succeeded.'
 
 file = open('numbers1.txt', 'w')
-numbers = '8 2'
+numbers = '-8 2'
 file.write(numbers)
 file.close()
 
@@ -25,14 +25,14 @@ cmdout = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
 print cmdout
 cmdouts = cmdout.split()
 result = int(cmdouts[-1])
-assert(result == 4)
+assert(result == -4)
 print 'Divider file input succeeded.'
 
 cmd = './calc multiplier numbers1 numbers2'
 cmdout = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
 cmdouts = cmdout.split()
 result = int(cmdouts[-1])
-assert(result == 16)
+assert(result == -16)
 print 'Multipliers file input succeeded.'
 
 
